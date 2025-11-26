bcc80b2ad8e09ce11d053f15b0cd530858c02af3
Farnaz <farnazboroumand84@gmail.com>
2025-11-26 15:50:34 +0330

    second commit

---
+++ backend/src/config/database.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URL);
+        console.log(`\n MongoDB connected...
+            ${connectionInstance.connection.host}`);  
+    }catch(error){
+        console.log(`\n Connection failed : ` , error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
